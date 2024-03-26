86cd63c29df98e4080ce07b4cfe177f07c4183ce
Laiba3909 <160255993+Laiba3909@users.noreply.github.com>
2024-03-26 14:19:19 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,18 @@
+import inquirer from "inquirer"
+
+const answer = await inquirer.prompt([
+    {message: "Enter your first number:", type: "number", name: "firstNumber"},
+    {message: "Enter your second number:", type: "number", name: "secondNumber"},
+    {message: "Enter operator:", type: "list", choices:["Addition","Subraction","Multiplication","Division"], name:"operator"},
+])
+
+if (answer.operator === "Addition"){
+    console.log(answer.firstNumber + answer.secondNumber)
+} else if (answer.operator === "Subraction"){
+    console.log(answer.firstNumber - answer.secondNumber)
+} else if (answer.operator === "Multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber)
+} else if (answer.operator === "Division"){
+    console.log(answer.firstNumber / answer.secondNumber)
+}
+   console.log("please enter correct operator")
